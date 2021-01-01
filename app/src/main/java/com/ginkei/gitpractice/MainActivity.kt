448e9d4c76a002319b0314cdfb88d79c54f3dea2
Leandro Guina <guina86@gmail.com>
2021-01-01 00:44:00 -0300

    Added viewBinding

--- app/src/main/java/com/ginkei/gitpractice/MainActivity.kt
+++ app/src/main/java/com/ginkei/gitpractice/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ginkei.gitpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ginkei.gitpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textBase.text = "Happy 2021"
+
     }
 }
